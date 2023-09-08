d1f6824e7d7f2aa92377687e39ecff615603ecef
dianabreik <diana.sam341@gmail.com>
2023-09-08 11:39:30 +0200

    add record(person) to save the persons's informations

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+       Person person1= new Person("1","Jan",33);
+        System.out.println(person1);
     }
 }
