4eb3fb425881a9e391c05ff72fc60fdcbc7f5a63
dianabreik <diana.sam341@gmail.com>
2023-09-08 11:52:25 +0200

    print the introduction

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        //Schritt 5: Wechselt zurück zum Haupt-Branch (z.B. 'main' oder 'master') und führt neue Änderungen durch, committed und pusht diese.
+        System.out.println("people who live here");
     }
 }
